9ee6f046d7f742e2f0a6bb40cc0d124dac2876a7
Allen <allenvs@mail.ru>
2020-09-18 23:49:54 +0300

    cleaned

--- app.js
+++ app.js
@@ -4,12 +4,10 @@
 // input: x, y
 // output: minimun steps from x to y
 
-console.log(brokenDevice(6,13)); // 3
-// console.log(brokenDevice(2,3)); //-> 2 (*2, -1)
-// console.log(brokenDevice(5,8)); //-> 2 (-1, *2)
-// console.log(brokenDevice(3,8)); //-> 3 (-1, *2, *2)
-
-// 2,4,8,16,32,64,128,256,512,1024
+console.log(brokenDevice(3,10)); // 3
+console.log(brokenDevice(2,3)); //-> 2 (*2, -1)
+console.log(brokenDevice(5,8)); //-> 2 (-1, *2)
+console.log(brokenDevice(3,8)); //-> 3 (-1, *2, *2)
 
 function brokenDevice(x, y) {
     let ans = 0;
@@ -20,20 +18,16 @@ function brokenDevice(x, y) {
         if (y > x) {
             if (y%2 !== 0) {
                 y += 1;
-                console.log("odd y: +1");
             } else {
                 y /= 2;
-                console.log("even y: /2");
             };
             ans +=1;
         };
         // if y is smaller it's simple 
         if (y < x) {
         y += 1;
-        console.log("y < x: +1")
         ans +=1;
         };
-        console.log("y = ", y);
     };
     return ans;
 };
